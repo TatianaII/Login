7a0b01de930e8599875c0c729aa4f66a33b6e4cf
Tatiana Salegel Sanséau <tatianasalegel@ii.local>
2020-05-21 17:57:55 -0300

    21-5

--- src/components/Navegation.js
+++ src/components/Navegation.js
@@ -80,10 +80,6 @@ const Navegation = () =>{
                 <div class="col-md-12 mx-auto">
                   <p>The HTML5 video element uses an mp4 video as a source. Change the source video to add in your own background! The header text is vertically centered using flex utilities that are build into Bootstrap 4.</p>
                   <p>The overlay color can be changed by changing the <code>background-color</code> of the <code>.overlay</code> class in the CSS.</p>
-                  <p>Set the mobile fallback image in the CSS by changing the background image of the header element within the media query at the bottom of the CSS snippet.</p>
-                  <p class="mb-0">
-                    Created by <a href="https://startbootstrap.com">Start Bootstrap</a>
-                  </p>
                 </div>
               </div>
             </div>
